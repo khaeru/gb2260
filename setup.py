d89cc49e63105842a6fd9e3d82aad826752c8738
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2017-02-13 21:26:25 -0500

    remove 'jianfan' dependency

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(name='gb2260',
       description='GB/T 2260-2007 codes',
       install_requires=[
         'beautfulsoup',
-        'jianfan',
         ],
       tests_require=['pytest'],
       url='https://github.com/khaeru/gb2260',
