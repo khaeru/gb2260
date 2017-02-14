c05ef73eed760e847eccec3599df653eca1e5928
Paul Natsuo Kishimoto <mail@paul.kishimoto.name>
2017-02-13 21:13:46 -0500

    basic packaging for Travis/RTD

--- setup.py
+++ setup.py
@@ -4,12 +4,14 @@ from setuptools import setup, find_packages
 
 setup(name='gb2260',
       version='1',
-      description=('GB/T 2260-2007 codes'),
+      author='Paul Natsuo Kishimoto',
+      author_email='mail@paul.kishimoto.name',
+      description='GB/T 2260-2007 codes',
+      install_requires=[
+        'beautfulsoup',
+        'jianfan',
+        ],
+      tests_require=['pytest'],
       url='https://github.com/khaeru/gb2260',
       packages=find_packages(),
-      long_description=("GB/T 2260-2007 中华人民共和国行政区划代码 Codes for "
-                        "the administrative divisions of the People's Republic"
-                        " of China"),
-      license='GPLv3',
-      platforms=['any'],
       )
